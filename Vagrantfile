f7a8a443367a2a37b607f4d4ede48ba4211611ce
ltdat789 <ltdat789@gmail.com>
2018-06-30 12:40:28 +0700

    update vagrant

--- Vagrantfile
+++ Vagrantfile
@@ -1,7 +1,3 @@
-unless Vagrant.has_plugin?("vagrant-vbguest")
-  raise "please run 'vagrant plugin install vagrant-vbguest'"
-end
- 
 Vagrant.configure(2) do |config|
  
   #CentOS7のboxを指定
@@ -11,7 +7,7 @@ Vagrant.configure(2) do |config|
   config.vm.network "private_network", ip: "192.168.77.10"
  
   #ホストマシンとゲストマシンのディレクトリをマウント
-  config.vm.synced_folder "Code", "/var/www/html", create: true
+  config.vm.synced_folder ".", "/var/www/html", create: true
  
   #プロビジョニングの処理をprovision.shファイルにまとめる。
   config.vm.provision :shell, :path => "provision.sh"
--- provision.sh
+++ provision.sh
@@ -8,52 +8,28 @@ rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
  
 #nginxをインストール
 yum --enablerepo=epel install nginx -y
+ 
+# Set up nginx server
+sudo cp /vagrant/nginx/nginx.conf /etc/nginx/conf.d/site.conf
+sudo chmod 644 /etc/nginx/conf.d/site.conf
 
 #nginxの自動起動設定、起動
-systemctl enable nginx
-systemctl start nginx
-systemctl status nginx
- 
-#confファイルのサンプルを作成
-cat << EOT > /etc/nginx/conf.d/mylaravel.conf
-server {
-  listen 80;
-  server_name vagrant-test.local.com;
-  root /var/www/html/public;
-  index index.php index.html index.htm;
-
-  # '/'で始まる全てのURIに一致
-  location / {
-    # リクエストURI, /index.phpの順に処理を試みる
-    try_files $uri $uri/ /index.php?$query_string;
-  }
- 
-  location ~ \.php$ {
-    fastcgi_split_path_info ^(.+\.php)(/.+)$;
-
-    fastcgi_pass unix:/var/run/php-fpm.sock;
-    fastcgi_index index.php;
-    fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-    fastcgi_param PATH_INFO $fastcgi_path_info;
-
-    include fastcgi_params;
-  }
-}
-EOT
-#nginxの自動起動設定、起動
-systemctl reload nginx
+sudo systemctl enable nginx
+sudo systemctl start nginx
+sudo systemctl status nginx
  
 #PHP7とPHP-FPMなどその他extensionをインストール
-yum install --enablerepo=epel,remi-php70 php php-mbstring php-pear php-fpm php-mcrypt php-gd php-mysql -y
+yum install --enablerepo=epel,remi-php71 php php-mbstring php-pear php-fpm php-mcrypt php-gd php-mysql -y
+sudo yum install zip unzip php7.1-zip
  
 #PHP-FPMのuserとgroupをnginxに変更
 sed -i 's/user = apache/user = nginx/g' /etc/php-fpm.d/www.conf
 sed -i 's/group = apache/group = nginx/g' /etc/php-fpm.d/www.conf
  
 #PHP-FPMの自動起動設定、起動
-systemctl enable php-fpm
-systemctl start php-fpm
-systemctl status php-fpm
+sudo systemctl enable php-fpm
+sudo systemctl start php-fpm
+sudo systemctl status php-fpm
  
 #MySQLのインストール
 rpm -ivh http://dev.mysql.com/get/mysql57-community-release-el7-7.noarch.rpm
@@ -62,8 +38,8 @@ yum install mysql-community-server -y
 #VagrantではパスワードなしでMySQLのrootユーザのログインを許可したい
 echo skip-grant-tables >> /etc/my.cnf
 #MySQLの自動起動設定、起動
-systemctl enable mysqld.service
-systemctl start mysqld.service
+sudo systemctl enable mysqld.service
+sudo systemctl start mysqld.service
 #Basic認証使いたいのでインストール
 yum install httpd-tools -y
 #node.jsとnpmインストール
@@ -73,6 +49,9 @@ yum install npm --enablerepo=epel -y
 curl -sS https://getcomposer.org/installer | php -- --install-dir=/tmp
 mv /tmp/composer.phar /usr/local/bin/composer
 ln -s /usr/local/bin/composer /bin
+
+# Install Git
+yum install git -y
  
 #Vagrantでディレクトリをマウントすると403になるのでSELinuxを停止
 setenforce 0
